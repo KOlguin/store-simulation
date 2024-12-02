8e429e04c2b0984eb621bcc0991deabf0a9f40fb
KOlguin <kolguin@uc.cl>
2024-12-02 15:21:16 -0300

    Add CRUD base

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,27 @@
 const express = require('express')
 
+const routes = require('./src/routes/index')
+const database = require('./src/models/index')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+app.use(routes)
 
-app.listen(port, () => {
-  console.log(`Server listen on ${port}`);
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello world!')
+// })
+
+database.sequelize
+        .sync()
+        .then(() => {
+          console.log('Databse Connected')
+          app.listen(port, () => {
+            console.log(`Server listen on ${port}`)
+          })
+        })
+        .catch((err) => console.error('Unable to Connect to database:', err))
+
+// app.listen(port, () => {
+//   console.log(`Server listen on ${port}`);
+// });
