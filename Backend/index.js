ceb501194784cd1837d8f06de065ef6e459ff52c
KOlguin <kolguin@uc.cl>
2024-12-02 16:32:22 -0300

    Add customers CRUD

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,8 @@ const database = require('./src/models/index')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.use(routes)
 
 // app.get('/', (req, res) => {
---
+++ Backend/src/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+const customerRoutes = require('./customer')
+
+router.use('/customer', customerRoutes)
+
+module.exports = router
