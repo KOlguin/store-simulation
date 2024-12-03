8e37ac94f34b7122b4d006c95bbed6106994d184
KOlguin <kolguin@uc.cl>
2024-12-03 14:03:05 -0300

    Add dummy data to sales and inventory

---
+++ store-simulation-frontend/src/utils/DummyData/dummySales.js
@@ -0,0 +1,74 @@
+const sales = [
+    {
+      id: 1,
+      salesDate: new Date("2024-06-07"),
+      customerID: 2,
+      salesDescription: "Lorem ipsum",
+      total: 250000
+    },
+    {
+      id: 2,
+      salesDate: new Date ("2024-06-12"),
+      customerID: 5,
+      salesDescription: "Lorem ipsum",
+      total: 259636
+    },
+    {
+      id: 3,
+      salesDate: new Date ("2024-06-20"),
+      customerID: 2,
+      salesDescription: "Lorem ipsum",
+      total: 1000000
+    },
+    {
+      id: 4,
+      salesDate: new Date ("2024-06-21"),
+      customerID: 4,
+      salesDescription: "Lorem ipsum",
+      total: 25000
+    },
+    {
+      id: 5,
+      salesDate: new Date ("2024-06-28"),
+      customerID: 8,
+      salesDescription: "Lorem ipsum",
+      total: 6984237
+    },
+    {
+        id: 1,
+        salesDate: new Date ("2024-07-03"),
+        customerID: 9,
+        salesDescription: "Lorem ipsum",
+        total: 8963895
+    },
+    {
+    id: 2,
+    salesDate: new Date ("2024-07-04"),
+    customerID: 10,
+    salesDescription: "Lorem ipsum",
+    total: 31496
+    },
+    {
+    id: 3,
+    salesDate: new Date ("2024-07-15"),
+    customerID: 8,
+    salesDescription: "Lorem ipsum",
+    total: 2652843
+    },
+    {
+    id: 4,
+    salesDate: new Date ("2024-07-21"),
+    customerID: 9,
+    salesDescription: "Lorem ipsum",
+    total: 328969
+    },
+    {
+    id: 5,
+    salesDate: new Date ("2024-07-27"),
+    customerID: 10,
+    salesDescription: "Lorem ipsum",
+    total: 13697
+    }
+  ];
+
+export default sales;
