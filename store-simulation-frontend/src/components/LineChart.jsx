3c4ea23015ae2d99534e1386d74ab22ab4557865
KOlguin <kolguin@uc.cl>
2024-12-03 14:03:39 -0300

    Add sales chart with dummy data

---
+++ store-simulation-frontend/src/components/LineChart.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+function LineChart({ chartData }) {
+  return (
+    <div className="chart-container">
+      <h2 style={{ textAlign: "center" }}>Resumen de las Ventas del segundo semestre de 2024</h2>
+      <Line
+        data={chartData}
+        options={{
+          plugins: {
+            legend: {
+              display: false
+            }
+          },
+          scales: {
+            y: {
+              title: {
+                display: true,
+                text: 'Total de la Venta ($)'
+              }
+            },
+            x: {
+              title: {
+                display: true,
+                text: 'Fecha de la Venta'
+              }
+            }
+          }
+        }}
+      />
+    </div>
+  );
+}
+export default LineChart;
